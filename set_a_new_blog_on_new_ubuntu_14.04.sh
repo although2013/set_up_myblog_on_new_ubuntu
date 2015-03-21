961f340da51e98d7d465dd0fdb2adf704a240528
Ge Hao <althoughghgh@gmail.com>
2015-03-21 16:38:12 +0800

    c

--- set_a_new_blog_on_new_ubuntu_14.04.sh
+++ set_a_new_blog_on_new_ubuntu_14.04.sh
@@ -2,6 +2,12 @@
 
 # WARNING: only for ubuntu 14.04
 
+#..................
+#useradd -m deployme
+#adduser deploy sudo
+#passwd deploy
+#..................
+
 sudo apt-get update;
 sudo apt-get -y install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev;
 
@@ -11,19 +17,23 @@ curl -L https://get.rvm.io | bash -s stable;
 source ~/.rvm/scripts/rvm;
 rvm install 2.2.1;
 rvm use 2.2.1 --default;
+echo "-------ruby version-----ruby version------"
 ruby -v;
 
-
-sudo add-apt-repository ppa:chris-lea/node.js;
+echo "-------nodejs-----nodejs------"
+sudo add-apt-repository -y ppa:chris-lea/node.js;
 sudo apt-get update;
 sudo apt-get -y install nodejs;
 
 
+
+echo "-------RMagick-----RMagick------"
 sudo apt-get -y install libmagickwand-dev;
 
-
+###
 ###passenger
-
+###
+echo "-------Passenger-----Passenger------"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 561F9B9CAC40B2F7;
 
 sudo apt-get -y install apt-transport-https ca-certificates;
@@ -38,7 +48,7 @@ sudo apt-get -y install nginx-extras passenger;
 
 
 
-sudo sed -i "s/# passenger_root/passenger_root/" -i "s/# passenger_ruby.*/passenger_ruby \/home\/deployme\/.rvm\/wrappers\/default\/ruby;/" /etc/nginx/nginx.conf > /etc/nginx/nginx.conf2
+sudo sh -c 'sed -e "s/# passenger_root/passenger_root/" -e "s/# passenger_ruby.*/passenger_ruby \/home\/deployme\/.rvm\/wrappers\/default\/ruby;/" /etc/nginx/nginx.conf > /etc/nginx/nginx.conf2'
 
 sudo mv /etc/nginx/nginx.conf  /etc/nginx/nginx.conf_back
 sudo mv /etc/nginx/nginx.conf2 /etc/nginx/nginx.conf
@@ -55,15 +65,18 @@ git clone https://github.com/although2013/MyBlog.git master
 
 cd master
 
+#exit
+#su - dployme
+#cd /var/www/although.com/master
+
 ./script/setup_server.sh
 
 
 
 
+sudo rm /etc/nginx/sites-enabled/default
 
-rm /etc/nginx/sites-enabled/default
-
-touch /etc/nginx/sites-enabled/although.com.conf
+sudo touch /etc/nginx/sites-enabled/although.com.conf
 
 
 sudo sh -c "echo '''server {
@@ -77,4 +90,5 @@ sudo sh -c "echo '''server {
 sudo service nginx restart
 
 
+sudo apt-get autoremove;
 
